46a7f65f86ceed8c5d3adf03abfde4f31753b38d
Tomas Bencomo <napertj@gmail.com>
2017-07-03 00:26:37 -0700

    Added quit button, changed window size, added main method

--- frontend.py
+++ frontend.py
@@ -1,5 +1,9 @@
 # frontend.py - This file holds all of the GUI code
 # Author: Tomas Bencomo
+# Application : class - sets up and runs GUI commands
+# main() : initiate GUI code
+
+
 
 from Tkinter import *
 import Tkinter as tk 
@@ -14,6 +18,7 @@ class Application(tk.Frame):
         tk.Frame.__init__(self, master)
         self.grid()
         self.createWidgets()
+        self.master.title('Easy Data Analysis')
     # prompts user for file that will be analyzed
     # sets 'filepath' attribute to the file path for the analyzed file
     def get_filepath(self):
@@ -21,14 +26,31 @@ class Application(tk.Frame):
         self.filepath = tkFileDialog.askopenfilename(initialdir = "/",title = "Select data file",filetypes = (("csv files","*.csv"),("all files","*.*")))
         print self.filepath
         # future - once the file is selected, call back end analysis and then draw visualization onscreen
+        # Backend calls go here
+
+
     # places needed buttons onto the screen
     def createWidgets(self):
         self.openButton = tk.Button(self, text = "Open File", command = self.get_filepath)
-        self.openButton.grid()
+        self.openButton.grid(row = 0, column = 0)
         # This is just here to easily close the app during testing
         self.quitButton = tk.Button(self, text = "Quit", command = self.quit)
-        self.quitButton.grid(column = 5, row = 5)
+        self.quitButton.grid(column = 0, row = 1)
+        self.tempLabel = tk.Label(self, text = 'Placeholder for visualizations', font=("Helvetica", 40))
+        self.tempLabel.grid(column = 1, row = 0)
 
-app = Application()
-app.master.title('Easy Data Analysis')
-app.mainloop()
+
+
+
+
+
+def main():
+    root = Tk()
+    app = Application(root)
+    # app.master.title('Easy Data Analysis')
+    w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+    root.geometry("%dx%d+0+0" % (w * .7, h * .7))
+    root.mainloop()
+
+
+if __name__ == "__main__": main()
