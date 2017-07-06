03aa70d1183de87ef439d2123f3d77f4c2b83a18
Julien Dhouti <julien.dhouti@gmail.com>
2017-07-06 14:42:02 -0700

    Updated the name generator algorithm
    Won't crash in the event that the path of the file doesn't contain any "/"

--- src/file_name_generator.py
+++ src/file_name_generator.py
@@ -13,12 +13,18 @@ class Name_Generator:
         analysis: String -- the kind of analysis performed
         """
         name, counter = "", -5
-        while True:
-            # iterates through the string backwards
-            if filepath[counter] != "/":
-                name = name + filepath[counter]
-                counter -= 1
-            else:
-                break
+        # in case the file is already in the same directory, just use the same name
+        if "/" not in filepath:
+            name = filepath
+        else:   # otherwise, ob
+            while True:
+                # iterates through the string backwards
+                if filepath[counter] != "/":
+                    name = name + filepath[counter]
+                    counter -= 1
+                else:
+                    break
 
-        return name[::-1] + "_" + column + "_" + analysis + ".jpg"
+            name = name[::-1] # flip the name back around
+
+        return name + "_" + column + "_" + analysis + ".jpg"
