ef5c8dcce6032600f4c8784729b746a20e41a595
Julien Dhouti <julien.dhouti@gmail.com>
2017-07-06 16:56:12 -0700

    Improved the efficiency of the column data fetcher.
    If the column has already been analyzed, everytime the function is called on it, it will skip a new analysis and just returned the previous information.

--- src/column_info.py
+++ src/column_info.py
@@ -13,18 +13,28 @@ class Column_Info:
         column: String -- contains all of the information in of the given column
         path: String -- the string that indicates the path to the csv file
         """
+        # create a apanda object using the given filepath
         data = pd.read_csv(path)
-        try:
+        try:    # checks to see if the given column exists
             self.column = data[column]
         except:
             raise ValueError("Column could not be found!")
         self.type = ""
+        self.determined_type = False    # set to true if get_type was already run
 
     def get_type(self):
         """Will return the data type as a string of a given panda series."""
+        # checks to see if the type has already been determined to improve efficiency
+        if self.determined_type:
+            return self.type
+
+        # if self.determined_type == false, type has not been determined so initiate
+        # the analysis
         if 'int' in str(self.column.dtype) or 'float' in str(self.column.dtype):
             self.type = 'numerical'
+            self.determined_type = True
             return 'numerical'
         else:
             self.type = 'categorical'
+            self.determined_type = True
             return 'categorical'
